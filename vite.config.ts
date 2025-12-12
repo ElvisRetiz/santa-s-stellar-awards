910d1274e7f9f00d2e202f4715881867fe0c7004
Elvis Retiz <elvis.retiz@hotmail.com>
2025-12-12 03:53:40 -0600

    BBBBBBBBBBBBBBBB

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
+  base: "/santa-s-stellar-awards/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
